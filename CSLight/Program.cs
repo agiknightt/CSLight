ec5281b3475f2f6301984c3fb8e16b4ac8c6ffc3
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-08-28 22:19:08 +0700

    Initial commit

---
+++ CSLight/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSLight
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            float fractionalNumber = 55.9871234f;
+            double longfractionalNumber = 13.1234567891011;
+
+            string name = "Александр";            
+
+            bool TrueOrFalse = false;
+
+            byte age = 26;
+            sbyte mounth = 12;
+            short day = 28;            
+            uint hour = 24;
+            long minute = 60;
+            ulong sec = 60;
+            int year = 1993;
+
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
